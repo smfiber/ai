f07a3bd2cc024110ac1b24c55d8f66e0570d8c84
Andy <andy_walch@yahoo.com>
2025-08-22 07:04:03 -0400

    Update config.js

--- stocks/stocks-orig/config.js
+++ stocks/stocks-orig/config.js
@@ -1,5 +1,5 @@
 // --- App Version ---
-export const APP_VERSION = "13.3.0"; 
+export const APP_VERSION = "13.3.1"; 
 
 // --- Shared State ---
 // This object will hold all the application's shared state.
