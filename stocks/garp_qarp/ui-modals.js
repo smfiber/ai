af665c9aa0fa98501da88918ce8928f4ad8fa929
Andy <andy_walch@yahoo.com>
2025-10-12 12:09:42 -0400

    Update ui-modals.js

--- stocks/garp_qarp/ui-modals.js
+++ stocks/garp_qarp/ui-modals.js
@@ -450,6 +450,7 @@ export async function openRawDataViewer(ticker) {
                      </div>`;
         }).join('');
         
+        const deepDiveHtml = buildButtonHtml(deepDiveButtons);
         const copyIconSvg = `<svg class="w-5 h-5 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125H4.875A1.125 1.125 0 013.75 20.625V7.875c0-.621.504-1.125 1.125-1.125H6.75m9 9.375h3.375c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125h-9.75A1.125 1.125 0 006 9.375v9.75c0 .621.504 1.125 1.125 1.125h3.375m-3.75-9.375V6.125c0-.621.504-1.125 1.125-1.125h9.75c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-3.375" /></svg>`;
 
         const buildSynthesisButton = (reportType, buttonId, text) => `
